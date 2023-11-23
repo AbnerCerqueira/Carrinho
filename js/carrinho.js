6b31fe875679ca9699b6060aa14c3e7464ae5975
AbnerCerqueria <abner.cerqueira@etec.sp.gov.br>
2023-11-22 22:55:22 -0300

    exibe total e desin

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,23 +1,23 @@
-function insereBanco(){
-    const banco = [{id: Date.now(),nome: "Maçã", valor: 1},
-                    {id: Date.now(),nome: "Banana", valor :1},
-                    {id: Date.now(),nome: "Abacaxi", valor: 1},
-                    {id: Date.now(),nome: "Manga", valor: 1},
-                    {id: Date.now(),nome: "Laranja", valor: 1},
-                    {id: Date.now(),nome: "Morango", valor: 1}
-                ]
+function insereBanco() {
+    const banco = [{ id: Date.now(), nome: "Maçã", valor: 29.99 },
+    { id: Date.now(), nome: "Banana", valor: 5.99 },
+    { id: Date.now(), nome: "Abacaxi", valor: 9.99 },
+    { id: Date.now(), nome: "Manga", valor: 16.99 },
+    { id: Date.now(), nome: "Laranja", valor: 7.99 },
+    { id: Date.now(), nome: "Morango", valor: 19.99 }
+    ]
     const carrinho = []
 
     let bd = JSON.stringify(banco)
     let car = JSON.stringify(carrinho)
 
-    localStorage.setItem("bd",bd)
-    localStorage.setItem("car",car)
+    localStorage.setItem("bd", bd)
+    sessionStorage.setItem("car", car)
 }
 
 function Create(button) {
     const dados = JSON.parse(localStorage.getItem("bd"));
-    const carrinho = JSON.parse(localStorage.getItem("car"));
+    const carrinho = JSON.parse(sessionStorage.getItem("car"));
 
     let nome = button.previousElementSibling.value;
 
@@ -26,7 +26,6 @@ function Create(button) {
     for (let i = 0; i < carrinho.length; i++) {
         if (carrinho[i].nome == nome) {
             carrinho[i].quantidade++;
-            carrinho[i].valor += dados[i].valor;
             itemExistente = true;
             break;
         }
@@ -46,5 +45,38 @@ function Create(button) {
     const car = JSON.stringify(carrinho);
 
     localStorage.setItem("bd", ds);
-    localStorage.setItem("car", car);
+    sessionStorage.setItem("car", car);
+}
+function Exibe() {
+    const carrinho = JSON.parse(sessionStorage.getItem("car"));
+    let produto = document.querySelector("#nome").value
+    switch (produto) {
+        case ("Abacaxi"):
+            document.querySelector("#numero").innerHTML = carrinho[0].quantidade
+        case ("Banana"):
+            document.querySelector("#numero").innerHTML = carrinho[1].quantidade
+        case ("Laranja"):
+            document.querySelector("#numero").innerHTML = carrinho[2].quantidade
+        case ("Manga"):
+            document.querySelector("#numero").innerHTML = carrinho[3].quantidade
+        case ("Maça"):
+            document.querySelector("#numero").innerHTML = carrinho[4].quantidade
+        case ("Morango"):
+            document.querySelector("#numero").innerHTML = carrinho[5].quantidade
+    }
+    console.log(carrinho[0].quantidade)
+}
+function Total() {
+    const carrinho = JSON.parse(sessionStorage.getItem("car"));
+    let total = 0
+    for (i = 0; i < carrinho.length; i++) {
+        total += carrinho[i].valor * carrinho[i].quantidade
+    }
+    document.querySelector("#total-exibe").innerHTML = total
+    return total
+}
+function Fim() {
+    const carrinho = JSON.parse(sessionStorage.getItem("car"));
+    alert("TOTAL: R$ " + Total() + "\nOBRIGADO POR COMRAR\nVOLTE SEMPRE NO SACOLÃO DO JOÃO")
+    window.location.reload()
 }
